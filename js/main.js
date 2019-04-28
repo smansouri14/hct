6d24bef9320a6f70c3f0b1a9057528b8904f5bf9
mansouri <mansouri@email.arizona.edu>
2019-04-28 14:32:40 -0700

    contact form

--- js/main.js
+++ js/main.js
@@ -1,27 +1,24 @@
 // Sticky Menu Backgroud opacity
-window.addEventListener('scroll', function() {
-    if (window.scrollY > 150) {
-      document.querySelector('#navbar').style.opacity = 0.9;
-    } else {
-      document.querySelector('#navbar').style.opacity = 1;
-    }
-  });
-
-
-
+window.addEventListener("scroll", function() {
+  if (window.scrollY > 150) {
+    document.querySelector("#navbar").style.opacity = 0.9;
+  } else {
+    document.querySelector("#navbar").style.opacity = 1;
+  }
+});
 
 // Smooth Scrolling //
-$('#navbar a, .btn').on('click', function(event) {
-    if (this.hash !== '') {
-        event.preventDefault();
+$("#navbar a, .btn").on("click", function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
 
-        const hash = this.hash;
+    const hash = this.hash;
 
-        $('html, body').animate(
-            {
-                scrollTop: $(hash).offset().top -150
-            },
-            800
-        );
-    }
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top - 150
+      },
+      800
+    );
+  }
 });
